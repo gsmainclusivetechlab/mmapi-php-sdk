74f474bb4517d3542c40205368803e309c8d58e6
Vishnu Haridas <vishnu.haridas@fingent.com>
2021-10-20 17:38:42 +0530

    Added Payment Refund API

---
+++ sample/MerchantPayment/PaymentRefund.php
@@ -0,0 +1,34 @@
+<?php
+use mmpsdk\MerchantPayment\Models\MerchantTransaction;
+use mmpsdk\Common\Constants\MobileMoney;
+use mmpsdk\Common\Enums\SecurityLevel;
+use mmpsdk\Common\Exceptions\SDKException;
+use mmpsdk\MerchantPayment\Process\PaymentRefund;
+
+MobileMoney::initialize(
+    MobileMoney::SANDBOX,
+    '59vthmq3f6i15v6jmcjskfkmh',
+    'ef8tl4gihlpfd7r8jpc1t1nda33q5kcnn32cj375lq6mg2nv7rb',
+    'oVN89kXyTx1cKT3ZohH7h6foEmQmjqQm3OK2U8Ue'
+);
+MobileMoney::setCallbackUrl(
+    'https://1527dea3-111f-48de-ba27-1c840df261c1.mock.pstmn.io/callback'
+);
+MobileMoney::setSecurityLevel(SecurityLevel::STANDARD);
+
+
+$transaction = new MerchantTransaction();
+$transaction
+    ->setAmount('200.00')
+    ->setCurrency('RWF')
+    ->setCreditParty(['accountid' => '2999'])
+    ->setDebitParty(['accountid' => '2999']);
+
+try {
+    $request = PaymentRefund::build($transaction);
+    print_r($request->getClientCorrelationId());
+    $repsonse = $request->execute();
+    print_r($repsonse);
+} catch (SDKException $ex) {
+    print_r($ex->getErrorObj());
+}
