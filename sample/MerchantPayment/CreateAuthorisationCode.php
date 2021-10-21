b7a750f6d77a431f7681c6a41278e84de17be3be
Vishnu Haridas <vishnu.haridas@fingent.com>
2021-10-21 14:07:48 +0530

    Refactoring Sample Code

--- sample/MerchantPayment/CreateAuthorisationCode.php
+++ sample/MerchantPayment/CreateAuthorisationCode.php
@@ -1,22 +1,7 @@
 <?php
-use mmpsdk\MerchantPayment\Models\MerchantTransaction;
-use mmpsdk\Common\Constants\MobileMoney;
-use mmpsdk\Common\Enums\SecurityLevel;
 use mmpsdk\Common\Exceptions\SDKException;
 use mmpsdk\MerchantPayment\Models\AuthorisationCode;
 use mmpsdk\MerchantPayment\Process\CreateAuthorisationCode;
-use mmpsdk\MerchantPayment\Process\InitiatePayment;
-
-MobileMoney::initialize(
-    MobileMoney::PRODUCTION,
-    '59vthmq3f6i15v6jmcjskfkmh',
-    'ef8tl4gihlpfd7r8jpc1t1nda33q5kcnn32cj375lq6mg2nv7rb',
-    'oVN89kXyTx1cKT3ZohH7h6foEmQmjqQm3OK2U8Ue'
-);
-MobileMoney::setCallbackUrl(
-    'https://1527dea3-111f-48de-ba27-1c840df261c1.mock.pstmn.io/callback'
-);
-MobileMoney::setSecurityLevel(SecurityLevel::STANDARD);
 
 $authorisationObj = new AuthorisationCode();
 $authorisationObj
--- sample/run.php
+++ sample/run.php
@@ -1,5 +1,19 @@
 <?php
 $loader = require dirname(__DIR__, 1) . '/vendor/autoload.php';
+use mmpsdk\Common\Constants\MobileMoney;
+use mmpsdk\Common\Enums\SecurityLevel;
+
+MobileMoney::initialize(
+    MobileMoney::PRODUCTION,
+    '59vthmq3f6i15v6jmcjskfkmh',
+    'ef8tl4gihlpfd7r8jpc1t1nda33q5kcnn32cj375lq6mg2nv7rb',
+    'oVN89kXyTx1cKT3ZohH7h6foEmQmjqQm3OK2U8Ue'
+);
+MobileMoney::setCallbackUrl(
+    'https://1527dea3-111f-48de-ba27-1c840df261c1.mock.pstmn.io/callback'
+);
+MobileMoney::setSecurityLevel(SecurityLevel::STANDARD);
+
 if (sizeof($argv) == 3) {
     $folderName = $argv[1];
     $fileName = $argv[2];
