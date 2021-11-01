47bc15aec1a0d5c553f9366fcc4deba93322a018
Vishnu Haridas <vishnu.haridas@fingent.com>
2021-11-01 13:11:49 +0530

    Added API for retrieving batch transaction status

---
+++ src/Disbursement/Process/RetrieveBatchTransaction.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace mmpsdk\Disbursement\Process;
+
+use mmpsdk\Common\Models\RequestState;
+use mmpsdk\Common\Utils\RequestUtil;
+
+use mmpsdk\Common\Utils\CommonUtil;
+use mmpsdk\Common\Constants\Header;
+use mmpsdk\Common\Constants\API;
+use mmpsdk\Common\Process\BaseProcess;
+use mmpsdk\Disbursement\Models\BatchTransaction;
+use mmpsdk\Disbursement\Models\DisbursementTransaction;
+
+/**
+ * Class RetrieveBatchTransaction
+ * @package mmpsdk\Disbursement\Process
+ */
+class RetrieveBatchTransaction extends BaseProcess
+{
+    /**
+     * Batch id
+     *
+     * @var string
+     */
+    private $batchId;
+
+    /**
+     * Gets the current status of a batch transaction
+     *
+     * @param string $batchId
+     * @return this
+     */
+    public function __construct(
+        $batchId
+    ) {
+        CommonUtil::validateArgument(
+            $batchId,
+            'batchId',
+            'string'
+        );
+        $this->setUp(self::SYNCHRONOUS_PROCESS);
+        $this->batchId = $batchId;
+        return $this;
+    }
+
+    public function execute()
+    {
+        $request = RequestUtil::get(
+            API::VIEW_BATCH_TRANSACTION
+        )
+            ->setUrlParams([
+                '{batchId}' => $this->batchId
+            ])
+            ->build();
+
+        $response = $this->makeRequest($request);
+        return $this->parseResponse($response, new BatchTransaction());
+    }
+}
