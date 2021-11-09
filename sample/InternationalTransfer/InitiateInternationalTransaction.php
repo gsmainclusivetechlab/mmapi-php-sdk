c41af2b8775e9ba939b46f84be6470de31377575
Vishnu Haridas <vishnu.haridas@fingent.com>
2021-11-09 18:15:24 +0530

    Added International Transaction API

--- sample/InternationalTransfer/InitiateInternationalTransaction.php
+++ sample/InternationalTransfer/InitiateInternationalTransaction.php
@@ -18,61 +18,61 @@ $transaction = new Transaction();
 $transaction
     ->setCreditParty(['accountid' => '2000'])
     ->setDebitParty(['accountid' => '2999'])
-    ->setAmount("100.00")
-    ->setCurrency("GBP");
+    ->setAmount('100.00')
+    ->setCurrency('GBP');
 $postalAddress = new Address();
 $postalAddress
-    ->setCountry("GB")
-    ->setAddressLine1("111 ABC Street")
-    ->setCity("New York")
-    ->setStateProvince("New York")
-    ->setPostalCode("ABCD");
+    ->setCountry('GB')
+    ->setAddressLine1('111 ABC Street')
+    ->setCity('New York')
+    ->setStateProvince('New York')
+    ->setPostalCode('ABCD');
 $subjectName = new Name();
 $subjectName
-    ->setTitle("Mr")
-    ->setFirstName("Luke")
-    ->setMiddleName("R")
-    ->setLastName("Skywalker")
-    ->setFullName("Luke R Skywalker")
-    ->setNativeName("ABC");
+    ->setTitle('Mr')
+    ->setFirstName('Luke')
+    ->setMiddleName('R')
+    ->setLastName('Skywalker')
+    ->setFullName('Luke R Skywalker')
+    ->setNativeName('ABC');
 $idDocument = [
     (new IdDocument())
-        ->setIdType("nationalidcard")
-        ->setIdNumber("1234567")
-        ->setIssueDate("2018-07-03T11:43:27.405Z")
-        ->setExpiryDate("2021-07-03T11:43:27.405Z")
-        ->setIssuer("UKPA")
-        ->setIssuerPlace("GB")
-        ->setIssuerCountry("GB")
-        ->setOtherIdDescription("test")
+        ->setIdType('nationalidcard')
+        ->setIdNumber('1234567')
+        ->setIssueDate('2018-07-03T11:43:27.405Z')
+        ->setExpiryDate('2021-07-03T11:43:27.405Z')
+        ->setIssuer('UKPA')
+        ->setIssuerPlace('GB')
+        ->setIssuerCountry('GB')
+        ->setOtherIdDescription('test')
 ];
 $senderKyc = new KYCInformation();
 $senderKyc
-    ->setNationality("GB")
-    ->setDateOfBirth("1970-07-03T11:43:27.405Z")
-    ->setOccupation("Manager")
-    ->setEmployerName("MFX")
-    ->setContactPhone("+447125588999")
-    ->setGender("m")
-    ->setEmailAddress("luke.skywalkeraaabbb@gmail.com")
-    ->setBirthCountry("GB")
+    ->setNationality('GB')
+    ->setDateOfBirth('1970-07-03T11:43:27.405Z')
+    ->setOccupation('Manager')
+    ->setEmployerName('MFX')
+    ->setContactPhone('+447125588999')
+    ->setGender('m')
+    ->setEmailAddress('luke.skywalkeraaabbb@gmail.com')
+    ->setBirthCountry('GB')
     ->setIdDocument($idDocument)
     ->setPostalAddress($postalAddress)
     ->setSubjectName($subjectName);
 $requestingOrganisation = new RequestingOrganisation();
 $requestingOrganisation
-    ->setRequestingOrganisationIdentifierType("organisationid")
-    ->setRequestingOrganisationIdentifier("testorganisation");
+    ->setRequestingOrganisationIdentifierType('organisationid')
+    ->setRequestingOrganisationIdentifier('testorganisation');
 $internationalTransferInformation = new InternationalTransferInformation();
 $internationalTransferInformation
-    ->setOriginCountry("GB")
-    ->setQuotationReference("REF-1636455454212")
+    ->setOriginCountry('GB')
+    ->setQuotationReference('REF-1636455454212')
     // ->setQuoteId("123}")
-    ->setReceivingCountry("RW")
-    ->setRemittancePurpose("personal")
-    ->setRelationshipSender("none")
-    ->setDeliveryMethod("agent")
-    ->setSendingServiceProviderCountry("AD");
+    ->setReceivingCountry('RW')
+    ->setRemittancePurpose('personal')
+    ->setRelationshipSender('none')
+    ->setDeliveryMethod('agent')
+    ->setSendingServiceProviderCountry('AD');
 $transaction
     ->setSenderKyc($senderKyc)
     ->setRequestingOrganisation($requestingOrganisation)
@@ -87,3 +87,4 @@ try {
     prettyPrint($ex->getMessage());
     prettyPrint($ex->getErrorObj());
 }
+
