4af3da08413cfe45e77b274c14a1dd14eca4e657
Vishnu Haridas <vishnu.haridas@fingent.com>
2021-11-30 13:30:53 +0530

    Added Docs for Recurring Payment

--- tests/src/RecurringPayment/RecurringPaymentTest.php
+++ tests/src/RecurringPayment/RecurringPaymentTest.php
@@ -10,6 +10,80 @@ class RecurringPaymentTest extends WrapperTestCase
     {
         $this->wrapperClass = RecurringPayment::class;
 
+        $requestState = RecurringPayment::viewRequestState('ABC123');
+        $this->checkStaticFunctionParamCount(
+            'viewRequestState',
+            mmpsdk\Common\Process\PollRequest::class
+        );
+        $this->checkFunctionReturnInstance(
+            $requestState,
+            mmpsdk\Common\Process\PollRequest::class
+        );
+
+        $response = RecurringPayment::viewResponse('ABC123');
+        $this->checkStaticFunctionParamCount(
+            'viewResponse',
+            mmpsdk\Common\Process\RetrieveMissingResponse::class
+        );
+        $this->checkFunctionReturnInstance(
+            $response,
+            mmpsdk\Common\Process\RetrieveMissingResponse::class
+        );
+
+        $serviceAvailability = RecurringPayment::viewServiceAvailability('ABC123');
+        $this->checkStaticFunctionParamCount(
+            'viewServiceAvailability',
+            mmpsdk\Common\Process\ServiceAvailability::class
+        );
+        $this->checkFunctionReturnInstance(
+            $serviceAvailability,
+            mmpsdk\Common\Process\ServiceAvailability::class
+        );
+
+        $accountBalance = RecurringPayment::viewAccountBalance([
+            'accountid' => 2000
+        ]);
+        $this->checkStaticFunctionParamCount(
+            'viewAccountBalance',
+            mmpsdk\Common\Process\AccountBalance::class
+        );
+        $this->checkFunctionReturnInstance(
+            $accountBalance,
+            mmpsdk\Common\Process\AccountBalance::class
+        );
+
+        $accountTransactions = RecurringPayment::viewAccountTransactions([
+            'accountid' => 2000
+        ]);
+        $this->checkStaticFunctionParamCount(
+            'viewAccountTransactions',
+            mmpsdk\Common\Process\RetrieveAccountTransactions::class
+        );
+        $this->checkFunctionReturnInstance(
+            $accountTransactions,
+            mmpsdk\Common\Process\RetrieveAccountTransactions::class
+        );
+
+        $retrieveTransaction = RecurringPayment::viewTransaction('ABC123');
+        $this->checkStaticFunctionParamCount(
+            'viewTransaction',
+            mmpsdk\Common\Process\RetrieveTransaction::class
+        );
+        $this->checkFunctionReturnInstance(
+            $retrieveTransaction,
+            mmpsdk\Common\Process\RetrieveTransaction::class
+        );
+
+        $reversalTransaction = RecurringPayment::createReversal('ABC123');
+        $this->checkStaticFunctionParamCount(
+            'createReversal',
+            mmpsdk\Common\Process\TransactionReversal::class
+        );
+        $this->checkFunctionReturnInstance(
+            $reversalTransaction,
+            mmpsdk\Common\Process\TransactionReversal::class
+        );
+
         $debitMandate = RecurringPayment::createAccountDebitMandate(
             ['accountid' => '2000'],
             new DebitMandate()
