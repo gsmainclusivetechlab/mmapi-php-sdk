b0c6edc1492c8aa1a2236983bf6db64ec0edbb5f
Vishnu Haridas <vishnu.haridas@fingent.com>
2021-12-06 10:18:47 +0530

    Removed api key

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,7 +3,7 @@ require dirname(__DIR__, 1) . '/autoload.php';
 use mmpsdk\Common\Constants\MobileMoney;
 error_reporting(E_ALL);
 ini_set('display_errors', '1');
-$ini = parse_ini_file( dirname(__DIR__, 1) . '/sdk-test-config.ini');
+$ini = parse_ini_file(dirname(__DIR__, 1) . '/sdk-test-config.ini');
 // To suppress the warning during the date() invocation in logs, we would default the timezone to GMT.
 if (!ini_get('date.timezone')) {
     date_default_timezone_set('GMT');
