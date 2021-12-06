35abbcf24b0adb1f88bdd44145ed84c3690a7ba7
Vishnu Haridas <vishnu.haridas@fingent.com>
2021-12-06 10:18:26 +0530

    Removed api key

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,13 +3,14 @@ require dirname(__DIR__, 1) . '/autoload.php';
 use mmpsdk\Common\Constants\MobileMoney;
 error_reporting(E_ALL);
 ini_set('display_errors', '1');
+$ini = parse_ini_file( dirname(__DIR__, 1) . '/sdk-test-config.ini');
 // To suppress the warning during the date() invocation in logs, we would default the timezone to GMT.
 if (!ini_get('date.timezone')) {
     date_default_timezone_set('GMT');
 }
 MobileMoney::initialize(
     MobileMoney::SANDBOX,
-    '59vthmq3f6i15v6jmcjskfkmh',
-    'ef8tl4gihlpfd7r8jpc1t1nda33q5kcnn32cj375lq6mg2nv7rb',
-    'oVN89kXyTx1cKT3ZohH7h6foEmQmjqQm3OK2U8Ue'
+    $ini['consumer_key'],
+    $ini['consumer_secret'],
+    $ini['api_key']
 );
