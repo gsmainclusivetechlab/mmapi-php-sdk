1678ca693f6c36a67e001ec71bab4722b6b253e5
Vishnu Haridas <vishnu.haridas@fingent.com>
2021-12-29 17:47:20 +0530

    Agent Service Sample Fix

--- sample/AgentService/RetrieveAccount.php
+++ sample/AgentService/RetrieveAccount.php
@@ -3,7 +3,6 @@ require_once __DIR__ . './../bootstrap.php';
 
 use mmpsdk\AgentService\AgentService;
 use mmpsdk\Common\Exceptions\SDKException;
-use mmpsdk\Common\Process\RetrieveAccount;
 
 try {
     $response = AgentService::viewAccount([
--- sample/AgentService/UpdateAccountIdentity.php
+++ sample/AgentService/UpdateAccountIdentity.php
@@ -1,18 +1,21 @@
 <?php
 require_once __DIR__ . './../bootstrap.php';
+
+use mmpsdk\AgentService\AgentService;
 use mmpsdk\Common\Exceptions\SDKException;
-use mmpsdk\Disbursement\Disbursement;
 use mmpsdk\Common\Models\PatchData;
 
 try {
+    $accountIdentifier = ['accountid' => '2000'];
     $patchRequest = new PatchData();
     $patchRequest
         ->setOp(PatchData::REPLACE)
-        ->setPath('/batchStatus')
-        ->setValue('approved');
-    $request = Disbursement::updateBatchTransaction(
+        ->setPath('/kycVerificationStatus')
+        ->setValue('verified');
+    $request = AgentService::updateAccountIdentity(
+        $accountIdentifier,
+        '105',
         [$patchRequest],
-        'REF-1635847150151'
     );
     prettyPrint($request->getClientCorrelationId());
     $repsonse = $request->execute();
